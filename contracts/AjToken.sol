2cc4c13140c23741c29dfaac408f1fb293793860
DTipan <ajas.bakran@gmail.com>
2018-09-23 14:57:36 +0530

    3. Delegated transfer

--- contracts/AjToken.sol
+++ contracts/AjToken.sol
@@ -10,6 +10,9 @@ contract AjToken {
 	string public symbol = "AJ";
 	string public standard = "AJ Token V1.0 standard";
 	mapping (address => uint256) public balanceOf;
+	mapping (address => mapping (address => uint)) public allowance;
+	
+	
 	uint256 public totalSupply;
 
 	// Transfer event
@@ -17,7 +20,14 @@ contract AjToken {
 		address indexed _from,
 		address indexed _to,
 		uint256 _value
-		);
+	);
+
+	// Aprove event
+	event Approval(
+		address indexed _owner,
+		address indexed _spender,
+		uint256 _value
+	);
 	
 	
 	//constructor
@@ -26,14 +36,7 @@ contract AjToken {
 		totalSupply = _initialSupply;
 	}	
     
-    
-	/*constructor (uint256 _initialSupply) public {
-		balanceOf[msg.sender] = _initialSupply; 
-		totalSupply = _initialSupply;
-		//allocate the totalSupply
-	}	*/
-
-	//transfer
+  	//transfer function
 
 	function transfer (address _to, uint256 _value) public returns(bool success) {
 		require (balanceOf[msg.sender] >= _value);		
@@ -44,6 +47,35 @@ contract AjToken {
 		return true;
  	}
 	
+	// Delegated Transfer
+
+	//approve
+	function approve (address _spender, uint256 _value) public returns(bool success) {
+		allowance[msg.sender][_spender] = _value;
+		Approval(msg.sender, _spender, _value);
+		return true;
+	}
+	
+	//transferFrom
+	function transferFrom (address _from, address _to, uint256 _value) public returns(bool success) {
+		//Require _from has enough tokens, that is sender is having enough tokens		
+		require (_value <= balanceOf[_from]);		
+		// Require allowance is big enough , that is limit is available
+		require (_value <= allowance[_from][msg.sender]);
+		//change the balance > after transferring change the balance of receving and sending parties
+		balanceOf[_from] -= _value;
+		balanceOf[_to] += _value;
+		//call transfer event 
+		Transfer(_from, _to, _value);
+		//finally, return boolean
+
+		return true;
+	}
+	
+	//allowance 
+
+
+
 
 }
 
